cd1b3331377dffbb4b0f271c012b9a8286a82bf7
hagans <rucurrucucu@hotmail.com>
2020-11-04 16:34:37 +0100

    Add files via upload

---
+++ SaveableEditor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEditor;
+
+namespace Hagans.Editor
+{
+    /// <summary>
+    /// Methods indorder to get serialized data from <see cref="SaveableEditorWindow{T}"/>.
+    /// </summary>
+    public static class SaveableEditor
+    {
+        /// <summary>
+        /// Denotes the existence of an entry at <see cref="EditorPrefs"/>. Overwrites result if possible.
+        /// </summary>
+        /// <typeparam name="TSaveable">Saveable editor window target.</typeparam>
+        /// <typeparam name="TData">Desired data type. Must match with data type.</typeparam>
+        /// <param name="data">Instance where data will be recorded. It must be non null.</param>
+        /// <returns>True if <see cref="EditorPrefs"/> serialized data wasn't null. Otherwise false.</returns>
+        /// <exception cref="ArgumentException">Data to overwrite and <see cref="EditorPrefs"/> serialized data has not the same <see cref="Type"/>.</exception>
+        public static bool TryGetValue<TSaveable, TData>(ref TData data) where TSaveable : SaveableEditorWindow<TData>
+        {
+            var json = EditorPrefs.GetString(typeof(TSaveable).Name);
+            if (string.IsNullOrEmpty(json))
+                return false;
+            try { EditorJsonUtility.FromJsonOverwrite(json, data); }
+            catch { throw new ArgumentException(typeof(TData).Name + " does not match with " + typeof(TSaveable)); }
+            return true;
+        }
+    }
+}
---
+++ SaveableEditorWindow.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEditor;
+
+namespace Hagans.Editor
+{
+    /// <summary>
+    /// EditorWindow with an attached class automatically stored at EditorPrefs.
+    /// </summary>
+    /// <typeparam name="T">Custom serializable class attached to this EditorWindow.</typeparam>
+    public abstract class SaveableEditorWindow<T> : EditorWindow
+    {
+        SerializedObject _window;
+        T _instanceData;
+
+        /// <summary>
+        /// This <see cref="EditorWindow"/> runtime <see cref="SerializedObject"/>.
+        /// </summary>
+        protected SerializedObject Window
+        {
+            get
+            {
+                if (_window == null) throw new NullReferenceException("Window is only avaiable when an instance is active.");
+                return _window;
+            }
+            set
+            {
+                if (_window == null) throw new NullReferenceException("Window is only avaiable when an instance is active.");
+                _window = value;
+            }
+        }
+
+        /// <summary>
+        /// Default value for T if <see cref="EditorPrefs"/> has nothing saved.
+        /// </summary>
+        protected abstract T InitialData { get; }
+
+        /// <summary>
+        /// T instance generated to be saved at <see cref="EditorPrefs"/> procedurally.
+        /// </summary>
+        protected abstract T OnSave { get; }
+
+
+        /// <summary>
+        /// Action to do with EditorPrefs T data when loaded.
+        /// </summary>
+        /// <param name="data"></param>
+        protected abstract void OnLoad(T data);
+
+        /// <summary>
+        /// Unity message. Loads <see cref="Window"/> and updates runtime data. 
+        /// Nothing before base.OnEnable() will has a <see cref="Window"/> 
+        /// instance or an updated T data.
+        /// </summary>
+        protected virtual void OnEnable()
+        {
+            var json = EditorPrefs.GetString(GetType().Name);
+            if (!string.IsNullOrEmpty(json))
+            {
+                _instanceData = InitialData;
+                EditorJsonUtility.FromJsonOverwrite(json, _instanceData);
+                OnLoad(_instanceData);
+            }
+            _window = new SerializedObject(this);
+        }
+
+        /// <summary>
+        /// Unity message. Saves runtime T data to EditorPrefs. Nothing after base.OnEnable() will has a <see cref="Window"/> instance.
+        /// </summary>
+        protected virtual void OnDisable() => Save();
+
+        void Save()
+        {
+            Window.ApplyModifiedProperties();
+            var json = EditorJsonUtility.ToJson(OnSave);
+            EditorPrefs.SetString(GetType().Name, json);
+        }
+    }
+}
